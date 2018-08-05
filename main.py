0454148dfaa2eccc812c6f7041db16c47e38669d
Selman Ercan <ercan.s.selman@gmail.com>
2018-08-05 18:03:54 +0200

    Changed request header.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ home_location_latitude = 52.086280
 home_location_longitude = 4.887380
 
 request_headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+    'User-Agent': 'Mozilla/5.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Referer': 'https://cssspritegenerator.com',
     'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
