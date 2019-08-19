670ea2b4ebc798eeac70b94bca17277ce0150937
Selman Ercan <selman.ercan@kpn.com>
2019-08-19 15:05:09 +0200

    Renamed module

--- ui.py
+++ ui.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 
-# from plane_finder import process, get_flight_info
-from flight_radar.get_from_flight_radar import process, get_flight_info
+from flight_radar.flight_radar import process, get_flight_info
 
 app = Flask(__name__)
 
